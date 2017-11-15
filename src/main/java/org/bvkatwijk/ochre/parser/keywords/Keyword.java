4245e8fede1000029e908f80ff53507f32cca6ee
bvkatwijk <bvkatwijk@gmail.com>
2017-11-15 08:32:04 +0100

    Use parboiled example

---
+++ src/main/java/org/bvkatwijk/ochre/parser/keywords/Keyword.java
@@ -0,0 +1,54 @@
+package org.bvkatwijk.ochre.parser.keywords;
+
+import java.util.Arrays;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Keyword {
+
+	ASSERT("assert"),
+	BREAK("break"),
+	CASE("case"),
+	CATCH("catch"),
+	CLASS("class"),
+	CONST("const"),
+	CONTINUE("continue"),
+	DEFAULT("default"),
+	DO("do"),
+	ELSE("else"),
+	ENUM("enum"),
+	EXTENDS("extends"),
+	FINALLY("finally"),
+	FINAL("final"),
+	FOR("for"),
+	GOTO("goto"),
+	IF("if"),
+	IMPLEMENTS("implements"),
+	IMPORT("import"),
+	INTERFACE("interface"),
+	INSTANCEOF("instanceof"),
+	NEW("new"),
+	PACKAGE("package"),
+	RETURN("return"),
+	STATIC("static"),
+	SUPER("super"),
+	SWITCH("switch"),
+	SYNCHRONIZED("synchronized"),
+	THIS("this"),
+	THROWS("throws"),
+	THROW("throw"),
+	TRY("try"),
+	VOID("void"),
+	WHILE("while"),
+	;
+	
+	private final String string;
+	
+	public static Object[] getStrings() {
+         return Arrays.stream(values()).map(Keyword::getString).toArray();
+	}
+
+}
---
+++ src/main/java/org/bvkatwijk/ochre/parser/range/CharRanges.java
@@ -0,0 +1,21 @@
+package org.bvkatwijk.ochre.parser.range;
+
+import org.parboiled.BaseParser;
+import org.parboiled.Rule;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CharRanges {
+	
+	private final BaseParser<Object> parser;
+	
+	public Rule ZeroToNine() {
+		return parser.CharRange('0', '9');
+	}
+
+	public Rule CharLowerAToLowerZ() {
+		return parser.CharRange('a', 'z');
+	}
+
+}
