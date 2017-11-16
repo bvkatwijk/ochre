44e3161bc6d0dcfc49adfed19b48559745b56e38
Unknown <bvkatwijk@gmail.com>
2017-11-16 16:20:55 +0100

    Add OchreCompiler

--- src/main/java/org/bvkatwijk/ochre/compiler/OchreCompiler.java
+++ src/main/java/org/bvkatwijk/ochre/compiler/OchreCompiler.java
@@ -1,38 +1,20 @@
 package org.bvkatwijk.ochre.compiler;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.UncheckedIOException;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.util.TraceClassVisitor;
 
-//package org.bvkatwijk.ochre.compiler;
-//
-//public class Example {
-//
-//	private final String name;
-//
-//	public Example(String name) {
-//		this.name = name;
-//	}
-//
-//}
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class OchreCompiler {
 
-	public OchreCompiler(InputStream inputStream) {
-
-		ClassWriter classWriter = new ClassWriter(0);
-		TraceClassVisitor traceClassVisitor = new TraceClassVisitor(
-				classWriter,
-				new PrintWriter(System.out));
+	public byte[] compile(String source) {
+		ClassWriter classWriter = getInternalWriter();
+		TraceClassVisitor traceClassVisitor = getWriter(classWriter);
 		traceClassVisitor.visit(
 				Opcodes.V1_8,
 				Opcodes.ACC_PUBLIC,
@@ -48,24 +30,17 @@ public class OchreCompiler {
 				null);
 		traceClassVisitor.visitEnd();
 
-		writeToFile(classWriter.toByteArray());
+		return classWriter.toByteArray();
 	}
 
-	private void writeToFile(byte[] b) {
-		try {
-			FileUtils.writeByteArrayToFile(new File("build/Example.class"), b);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	private ClassWriter getInternalWriter() {
+		return new ClassWriter(0);
 	}
 
-	private static byte[] fileToByteArray(InputStream inputStream) {
-		try {
-			return IOUtils.toByteArray(inputStream);
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		}
+	private TraceClassVisitor getWriter(ClassWriter classWriter) {
+		return new TraceClassVisitor(
+				classWriter,
+				new PrintWriter(System.out));
 	}
 
 }
--- src/test/java/org/bvkatwijk/ochre/compiler/OchreCompilerTest.java
+++ src/test/java/org/bvkatwijk/ochre/compiler/OchreCompilerTest.java
@@ -1,12 +1,26 @@
 package org.bvkatwijk.ochre.compiler;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import net.bytebuddy.jar.asm.ClassReader;
 
 public class OchreCompilerTest {
 
+	private static final String source = ""
+			+ "\n" + "package org.bvkatwijk.ochre.compiler;"
+			+ "\n" + ""
+			+ "\n" + "class Example(name: String) {"
+			+ "\n" + ""
+			+ "\n" + "}";
+
+	private static final OchreCompiler COMPILER = new OchreCompiler();
+
 	@Test
-	public void testCompiler() {
-		new OchreCompiler(this.getClass().getResourceAsStream("Example.ochre"));
+	public void className_shouldBe_Example() {
+		Assert.assertEquals(
+				"org/bvkatwijk/ochre/compiler/Example",
+				new ClassReader(COMPILER.compile(source)).getClassName());
 	}
 
 }
