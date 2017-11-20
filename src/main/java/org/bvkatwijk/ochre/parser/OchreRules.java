72063600d9993cd12ad315bba72397a31b09835c
Unknown <bvkatwijk@gmail.com>
2017-11-21 00:06:38 +0100

    Set type to String

--- src/main/java/org/bvkatwijk/ochre/parser/OchreRules.java
+++ src/main/java/org/bvkatwijk/ochre/parser/OchreRules.java
@@ -9,7 +9,7 @@ import org.parboiled.annotations.MemoMismatches;
 import org.parboiled.annotations.SuppressNode;
 import org.parboiled.annotations.SuppressSubnodes;
 
-public class OchreRules extends BaseParser<Object> {
+public class OchreRules extends BaseParser<String> {
 
 	private final CharRanges ranges = new CharRanges(this);
 
