87195a354c1d95cc0e55884e4347b2ea1eee0402
Unknown <bvkatwijk@gmail.com>
2017-11-20 17:14:45 +0100

    Add debug logging to java compiler

--- src/main/java/org/bvkatwijk/ochre/compiler/java/OchreToJavaCompiler.java
+++ src/main/java/org/bvkatwijk/ochre/compiler/java/OchreToJavaCompiler.java
@@ -1,13 +1,45 @@
 package org.bvkatwijk.ochre.compiler.java;
 
 import org.bvkatwijk.ochre.compiler.OchreCompiler;
+import org.bvkatwijk.ochre.parser.OchreNewRules;
+import org.bvkatwijk.ochre.parser.OchreParser;
+import org.parboiled.Parboiled;
+import org.parboiled.errors.ErrorUtils;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.ParsingResult;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
 public class OchreToJavaCompiler implements OchreCompiler {
+
+	private final OchreParser parser;
+
+	public OchreToJavaCompiler() {
+		this(new OchreParser());
+	}
 
 	@Override
 	public String compile(String source) {
+		System.out.println(parse(source));
+
 		return ""
-				+ "\n" + "public " + source.trim();
+		+ "\n" + "public " + source.trim();
+	}
+
+	private ParsingResult<Object> parse(String source) {
+		ParsingResult<Object> parsingResult = new ReportingParseRunner<Object>(
+				Parboiled.createParser(OchreNewRules.class)
+				.CompilationUnit())
+				.run(source);
+		if(parsingResult.hasErrors()) {
+			log.error(ErrorUtils.printParseErrors(parsingResult));
+			throw new IllegalStateException(
+					"Parse errors found.");
+		}
+		return parsingResult;
 	}
 
 }
