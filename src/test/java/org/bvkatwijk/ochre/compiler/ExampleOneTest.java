5c08ce9989eb70b79fe430fcf0eb154bc6a954c0
Unknown <bvkatwijk@gmail.com>
2017-11-20 17:52:51 +0100

    Convert to java compile test, ignore for now

--- src/test/java/org/bvkatwijk/ochre/compiler/ExampleOneTest.java
+++ src/test/java/org/bvkatwijk/ochre/compiler/ExampleOneTest.java
@@ -1,27 +1,35 @@
 package org.bvkatwijk.ochre.compiler;
 
 import org.bvkatwijk.ochre.compiler.java.AbstractOchreToJavaCompilerTest;
+import org.junit.Ignore;
 
+@Ignore("TO DO")
 public class ExampleOneTest extends AbstractOchreToJavaCompilerTest {
 
-	private static final String source = ""
-			+ "\n" + "package org.bvkatwijk.ochre.compiler;"
-			+ "\n" + ""
-			+ "\n" + "class Example(name: String) {"
-			+ "\n" + ""
-			+ "\n" + "}";
+	@Override
+	public String ochre() {
+		return ""
+				+ "\n" + "package org.bvkatwijk.ochre.compiler;"
+				+ "\n" + ""
+				+ "\n" + "class Example(name: String) {"
+				+ "\n" + ""
+				+ "\n" + "}";
+	}
 
-	private static final String target = ""
-			+ "\n" + "package org.bvkatwijk.ochre.compiler;"
-			+ "\n" + ""
-			+ "\n" + "public class Example {"
-			+ "\n" + ""
-			+ "\n" + "\t" + "private final String name;"
-			+ "\n" + ""
-			+ "\n" + "\t" + "public Example(String name) {"
-			+ "\n" + "\t" + "\t" + "this.name = name;"
-			+ "\n" + "\t" + "}"
-			+ "\n" + ""
-			+ "\n" + "}";
+	@Override
+	public String java() {
+		return ""
+				+ "\n" + "package org.bvkatwijk.ochre.compiler;"
+				+ "\n" + ""
+				+ "\n" + "public class Example {"
+				+ "\n" + ""
+				+ "\n" + "\t" + "private final String name;"
+				+ "\n" + ""
+				+ "\n" + "\t" + "public Example(String name) {"
+				+ "\n" + "\t" + "\t" + "this.name = name;"
+				+ "\n" + "\t" + "}"
+				+ "\n" + ""
+				+ "\n" + "}";
+	}
 
 }
