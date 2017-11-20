acedd678d0a457b91fe550c3757206e53e6116d3
Unknown <bvkatwijk@gmail.com>
2017-11-21 00:05:06 +0100

    Add adjustable rule for compilation test

--- src/main/java/org/bvkatwijk/ochre/compiler/java/OchreToJavaCompiler.java
+++ src/main/java/org/bvkatwijk/ochre/compiler/java/OchreToJavaCompiler.java
@@ -1,8 +1,11 @@
 package org.bvkatwijk.ochre.compiler.java;
 
+import java.util.function.Function;
+
 import org.bvkatwijk.ochre.compiler.OchreCompiler;
 import org.bvkatwijk.ochre.parser.OchreRules;
 import org.parboiled.Parboiled;
+import org.parboiled.Rule;
 import org.parboiled.errors.ErrorUtils;
 import org.parboiled.parserunners.ReportingParseRunner;
 import org.parboiled.support.ParsingResult;
@@ -16,13 +19,16 @@ public class OchreToJavaCompiler implements OchreCompiler {
 
 	@Override
 	public String compile(String source) {
-		return parse(source).resultValue;
+		return parse(source, OchreRules::CompilationUnit).resultValue;
 	}
 
-	private ParsingResult<String> parse(String source) {
+	public String compile(String source, Function<OchreRules, Rule> ruleFunction) {
+		return parse(source, ruleFunction).resultValue;
+	}
+
+	private ParsingResult<String> parse(String source, Function<OchreRules, Rule> ruleFunction) {
 		ParsingResult<String> parsingResult = new ReportingParseRunner<String>(
-				Parboiled.createParser(OchreRules.class)
-				.CompilationUnit())
+				ruleFunction.apply(Parboiled.createParser(OchreRules.class)))
 				.run(source);
 		if(parsingResult.hasErrors()) {
 			log.error(ErrorUtils.printParseErrors(parsingResult));
