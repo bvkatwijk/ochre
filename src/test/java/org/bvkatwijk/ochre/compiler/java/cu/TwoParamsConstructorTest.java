0388b8082a6a24e9477a2c3ed73779a45de87fff
Unknown <bvkatwijk@gmail.com>
2017-11-21 12:32:44 +0100

    Add two params constructor test

---
+++ src/test/java/org/bvkatwijk/ochre/compiler/java/cu/TwoParamsConstructorTest.java
@@ -0,0 +1,31 @@
+package org.bvkatwijk.ochre.compiler.java.cu;
+
+import org.bvkatwijk.ochre.compiler.java.AbstractOchreToJavaCompilerTest;
+
+public class TwoParamsConstructorTest extends AbstractOchreToJavaCompilerTest {
+
+	@Override
+	public String ochre() {
+		return ""
+				+ "\n" + "class ClassName(one: TypeOne, two: TypeTwo) {"
+				+ "\n" + ""
+				+ "\n" + "}";
+	}
+
+	@Override
+	public String java() {
+		return ""
+				+ "\n" + "public class ClassName {"
+				+ "\n" + ""
+				+ "\n" + "\t" + "private final TypeOne one;"
+				+ "\n" + "\t" + "private final TypeTwo two;"
+				+ "\n" + ""
+				+ "\n" + "\t" + "public ClassName(TypeOne one, TypeTwo two) {"
+				+ "\n" + "\t" + "\t" + "this.one = one;"
+				+ "\n" + "\t" + "\t" + "this.two = two;"
+				+ "\n" + "\t" + "}"
+				+ "\n" + ""
+				+ "\n" + "}";
+	}
+
+}
