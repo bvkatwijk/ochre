52f276b4738647dddc262a9379c1723ea9e459bd
Boris van Katwijk <bvkatwijk@gmail.com>
2018-06-22 17:04:02 +0200

    Add more rules to char ranges

--- src/main/java/org/bvkatwijk/ochre/parser/range/CharRanges.java
+++ src/main/java/org/bvkatwijk/ochre/parser/range/CharRanges.java
@@ -2,6 +2,7 @@ package org.bvkatwijk.ochre.parser.range;
 
 import org.parboiled.BaseParser;
 import org.parboiled.Rule;
+import org.parboiled.annotations.MemoMismatches;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,12 +11,36 @@ public class CharRanges {
 
 	private final BaseParser<String> parser;
 
+	public Rule Digit() {
+		return ZeroToNine();
+	}
+
 	public Rule ZeroToNine() {
-		return parser.CharRange('0', '9');
+		return this.parser.CharRange('0', '9');
 	}
 
 	public Rule CharLowerAToLowerZ() {
-		return parser.CharRange('a', 'z');
+		return this.parser.CharRange('a', 'z');
+	}
+
+	public Rule CharUpperAToUpperZ() {
+		return this.parser.CharRange('A', 'Z');
+	}
+
+	public Rule Letter() {
+		return this.parser.FirstOf(
+				CharLowerAToLowerZ(),
+				CharUpperAToUpperZ(), '_', '$');
+	}
+
+	@MemoMismatches
+	public Rule LetterOrDigit() {
+		return this.parser.FirstOf(
+				CharLowerAToLowerZ(),
+				CharUpperAToUpperZ(),
+				ZeroToNine(),
+				'_',
+				'$');
 	}
 
 }
