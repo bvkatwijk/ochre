1b30163ab5b589d7a1a54a330c91a0d4512c5520
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-19 13:58:53 +0200

    Add test cases

--- src/test/java/org/bvkatwijk/ochre/lang/pack/PackageTest.java
+++ src/test/java/org/bvkatwijk/ochre/lang/pack/PackageTest.java
@@ -19,10 +19,44 @@ public class PackageTest {
 			}
 
 			@Test
-			public void package_shouldHaveIdenfitier_withName_A() {
+			public void package_shouldHaveIdenfitier_withName_a() {
 				Assert.assertEquals(
 						"a",
 						SINGLE.getPackageIdentifiers().get(0).getName());
+			}
+
+		}
+
+		public static class MultipleIdentifiers {
+
+			private static final Package MULTIPLE = Package.of("a", "b", "c");
+
+			@Test
+			public void package_shouldHaveOneIdenfitier() {
+				Assert.assertEquals(
+						3,
+						MULTIPLE.getPackageIdentifiers().size());
+			}
+
+			@Test
+			public void package_shouldHaveIdenfitier_withName_a() {
+				Assert.assertEquals(
+						"a",
+						MULTIPLE.getPackageIdentifiers().get(0).getName());
+			}
+
+			@Test
+			public void package_shouldHaveIdenfitier_withName_b() {
+				Assert.assertEquals(
+						"b",
+						MULTIPLE.getPackageIdentifiers().get(1).getName());
+			}
+
+			@Test
+			public void package_shouldHaveIdenfitier_withName_c() {
+				Assert.assertEquals(
+						"c",
+						MULTIPLE.getPackageIdentifiers().get(2).getName());
 			}
 
 		}
