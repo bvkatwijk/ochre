ae6956877150d41658ce6c0a809aad021abc2dc6
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-19 14:10:01 +0200

    Rework importtests

--- src/test/java/org/bvkatwijk/ochre/parser/imp/ImportStatementParserTest.java
+++ src/test/java/org/bvkatwijk/ochre/parser/imp/ImportStatementParserTest.java
@@ -9,81 +9,93 @@ import org.parboiled.errors.ParsingException;
 
 public class ImportStatementParserTest extends BaseImportStatementParserTest {
 
-	@Test
-	public void testimport_A() {
-		Assert.assertEquals(
-				List.of(new Import("A")),
-				compile("import A;"));
+	public static class TypeOnly extends BaseImportStatementParserTest {
+
+		@Test
+		public void testimport_A() {
+			Assert.assertEquals(
+					List.of(new Import("A")),
+					compile("import A;"));
+		}
+
+		@Test
+		public void testimport_B() {
+			Assert.assertEquals(
+					List.of(new Import("B")),
+					compile("import B;"));
+		}
+
+		@Test
+		public void testimport_Type() {
+			Assert.assertEquals(
+					List.of(new Import("Type")),
+					compile("import Type;"));
+		}
 	}
 
-	@Test
-	public void testimport_B() {
-		Assert.assertEquals(
-				List.of(new Import("B")),
-				compile("import B;"));
+	public static class SingleQualifiedType extends BaseImportStatementParserTest {
+
+		@Test
+		public void testimport_aDotB() {
+			Assert.assertEquals(
+					List.of(new Import("a.B")),
+					compile("import a.B;"));
+		}
+
+		@Test
+		public void testimport_aDotBDotC() {
+			Assert.assertEquals(
+					List.of(new Import("a.b.C")),
+					compile("import a.b.C;"));
+		}
 	}
 
-	@Test
-	public void testimport_Type() {
-		Assert.assertEquals(
-				List.of(new Import("Type")),
-				compile("import Type;"));
+	public static class CommaSeperatedImports extends BaseImportStatementParserTest {
+
+		@Test
+		public void testTwoImports_A_B() {
+			Assert.assertEquals(
+					List.of(new Import("A"), new Import("B")),
+					compile("import A, B;"));
+		}
+
+		@Test
+		public void testThreeImports_A_B_C() {
+			Assert.assertEquals(
+					List.of(new Import("A"), new Import("B"), new Import("C")),
+					compile("import A, B, C;"));
+		}
 	}
 
-	@Test
-	public void testimport_aDotB() {
-		Assert.assertEquals(
-				List.of(new Import("a.B")),
-				compile("import a.B;"));
-	}
+	public static class BraceImports extends BaseImportStatementParserTest {
 
-	@Test
-	public void testimport_aDotBDotC() {
-		Assert.assertEquals(
-				List.of(new Import("a.b.C")),
-				compile("import a.b.C;"));
-	}
+		@Test
+		public void testBraceImport_A() {
+			Assert.assertEquals(
+					List.of(new Import("A")),
+					compile("import { A };"));
+		}
 
-	@Test
-	public void testTwoImports_A_B() {
-		Assert.assertEquals(
-				List.of(new Import("A"), new Import("B")),
-				compile("import A, B;"));
-	}
+		@Test
+		public void testBraceImport_aDotB() {
+			Assert.assertEquals(
+					List.of(new Import("a.B")),
+					compile("import { a.B };"));
+		}
 
-	@Test
-	public void testThreeImports_A_B_C() {
-		Assert.assertEquals(
-				List.of(new Import("A"), new Import("B"), new Import("C")),
-				compile("import A, B, C;"));
-	}
+		@Test
+		public void testBraceTwoImports_A_B() {
+			Assert.assertEquals(
+					List.of(new Import("A"), new Import("B")),
+					compile("import { A, B };"));
+		}
 
-	@Test
-	public void testBraceImport_A() {
-		Assert.assertEquals(
-				List.of(new Import("A")),
-				compile("import { A };"));
-	}
-
-	@Test
-	public void testBraceImport_aDotB() {
-		Assert.assertEquals(
-				List.of(new Import("a.B")),
-				compile("import { a.B };"));
-	}
-
-	@Test
-	public void testBraceTwoImports_A_B() {
-		Assert.assertEquals(
-				List.of(new Import("A"), new Import("B")),
-				compile("import { A, B };"));
-	}
-
-	@Test
-	public void testImport_elaborateCase() {
-		Assert.assertEquals(
-				List.of(new Import("a.B"), new Import("c.D"), new Import("E")),
-				compile("import { a.B, c.D }, E;"));
+		@Test
+		public void testImport_elaborateCase() {
+			Assert.assertEquals(
+					List.of(new Import("a.B"), new Import("c.D"), new Import("E")),
+					compile("import { a.B, c.D }, E;"));
+		}
 	}
 
 	@Test(expected = ParsingException.class)
