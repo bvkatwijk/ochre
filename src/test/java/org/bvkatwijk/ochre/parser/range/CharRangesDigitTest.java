c4bfc7c8a3b10d71c868f6b3cad9e89641349258
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-19 10:20:40 +0200

    Add tests for ranges

--- src/test/java/org/bvkatwijk/ochre/parser/range/CharRangesDigitTest.java
+++ src/test/java/org/bvkatwijk/ochre/parser/range/CharRangesDigitTest.java
@@ -18,6 +18,16 @@ public class CharRangesDigitTest extends BaseParserTest<String> {
 		Assert.assertEquals("1", compile("1"));
 	}
 
+	@Test
+	public void testDigit_8() {
+		Assert.assertEquals("8", compile("8"));
+	}
+
+	@Test
+	public void testDigit_9() {
+		Assert.assertEquals("9", compile("9"));
+	}
+
 	@Override
 	public Rule getRule() {
 		return Parboiled.createParser(CharRanges.class).Digit();
---
+++ src/test/java/org/bvkatwijk/ochre/parser/range/CharRangesLetterTest.java
@@ -0,0 +1,56 @@
+package org.bvkatwijk.ochre.parser.range;
+
+import org.bvkatwijk.ochre.compiler.java.cu.BaseParserTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.parboiled.Parboiled;
+import org.parboiled.Rule;
+import org.parboiled.errors.ParsingException;
+
+public class CharRangesLetterTest extends BaseParserTest<String> {
+
+	@Test
+	public void test_CharUpperAToUpperZ_A() {
+		Assert.assertEquals("A", compile("A"));
+	}
+
+	@Test
+	public void test_CharUpperAToUpperZ_B() {
+		Assert.assertEquals("B", compile("B"));
+	}
+
+	@Test
+	public void test_CharUpperAToUpperZ_Y() {
+		Assert.assertEquals("Y", compile("Y"));
+	}
+
+	@Test
+	public void test_CharUpperAToUpperZ_Z() {
+		Assert.assertEquals("Z", compile("Z"));
+	}
+
+	@Test(expected = ParsingException.class)
+	public void test_CharUpperAToUpperZ_1_throws() {
+		compile("1");
+	}
+
+	@Test(expected = ParsingException.class)
+	public void test_CharUpperAToUpperZ_z_throws() {
+		compile("a");
+	}
+
+	@Test(expected = ParsingException.class)
+	public void test_CharUpperAToUpperZ_a_throws() {
+		compile("z");
+	}
+
+	@Test(expected = ParsingException.class)
+	public void test_CharUpperAToUpperZ_underscore_throws() {
+		compile("_");
+	}
+
+	@Override
+	public Rule getRule() {
+		return Parboiled.createParser(CharRanges.class).CharUpperAToUpperZ();
+	}
+}
