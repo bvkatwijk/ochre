e46bb2988ead0498a8e80cbd05fd58fe90d11929
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-20 16:11:05 +0200

    Add conversion methods to package

--- src/main/java/org/bvkatwijk/ochre/lang/pack/Package.java
+++ src/main/java/org/bvkatwijk/ochre/lang/pack/Package.java
@@ -3,6 +3,7 @@ package org.bvkatwijk.ochre.lang.pack;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import lombok.Value;
 
@@ -19,6 +20,24 @@ public class Package {
 
 	public static Package of(PackageIdentifier... identifiers) {
 		return new Package(List.of(identifiers));
+	}
+
+	public String getValue() {
+		return this.packageIdentifiers
+				.stream()
+				.map(PackageIdentifier::getName)
+				.collect(Collectors.joining("."));
+	}
+
+	public Package prepend(Package parent) {
+		return new Package(
+				Stream.of(parent.getPackageIdentifiers(), this.packageIdentifiers)
+						.flatMap(List::stream)
+						.collect(Collectors.toList()));
+	}
+
+	public String getPrependValue() {
+		return this.getValue() == "" ? "" : this.getValue() + ".";
 	}
 
 }
