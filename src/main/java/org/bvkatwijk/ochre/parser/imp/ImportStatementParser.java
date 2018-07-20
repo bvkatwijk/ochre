e9ccb25187f1a15e3c83c4bb0000008ca6ac2c79
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-20 16:51:29 +0200

    Use import qualification parser in import statement parser

--- src/main/java/org/bvkatwijk/ochre/parser/imp/ImportStatementParser.java
+++ src/main/java/org/bvkatwijk/ochre/parser/imp/ImportStatementParser.java
@@ -1,80 +1,31 @@
 package org.bvkatwijk.ochre.parser.imp;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.bvkatwijk.ochre.lang.imp.Import;
-import org.bvkatwijk.ochre.parser.WhiteSpaceRules;
 import org.bvkatwijk.ochre.parser.keywords.KeywordParser;
-import org.bvkatwijk.ochre.parser.pack.PackageParser;
-import org.bvkatwijk.ochre.parser.range.CharRanges;
 import org.bvkatwijk.ochre.parser.symbol.SymbolParser;
-import org.bvkatwijk.ochre.parser.type.TypeReferenceParser;
 import org.parboiled.BaseParser;
 import org.parboiled.Parboiled;
 import org.parboiled.Rule;
-import org.parboiled.support.StringVar;
-import org.parboiled.support.Var;
 
-/**
- * Parser for import statements, i.e. import a.b { C, d.E }
- *
- * @author boris
- */
 public class ImportStatementParser extends BaseParser<List<Import>> {
 
+	public final ImportQualificationParser importQualificationParser = ImportQualificationParser.create();
 	public final KeywordParser keywordParser = KeywordParser.create();
 	public final SymbolParser symbolParser = Parboiled.createParser(SymbolParser.class);
-	public final WhiteSpaceRules whitespace = Parboiled.createParser(WhiteSpaceRules.class);
-	public final TypeReferenceParser type = Parboiled.createParser(TypeReferenceParser.class);
-	public final CharRanges ranges = Parboiled.createParser(CharRanges.class);
-	public final PackageParser packageParser = PackageParser.create();
 
 	public Rule ImportStatement() {
-		Var<List<Import>> children = new Var<>(new ArrayList<>());
 		return Sequence(
-				ImportStatementMatcher(children),
-				push(children.get()));
+				ImportStatementMatcher(),
+				push(this.importQualificationParser.pop().getChildren()));
 	}
 
-	public Rule ImportStatementMatcher(Var<List<Import>> children) {
+	public Rule ImportStatementMatcher() {
 		return Sequence(
 				this.keywordParser.Import(),
-				ImportQualification(children),
-				Optional(OneOrMore(ImportSeparator(), ImportQualification(children))),
+				this.importQualificationParser.RootImportQualification(),
 				this.symbolParser.Semicolon());
-	}
-
-	public Rule ImportQualification(Var<List<Import>> children) {
-		StringVar parent = new StringVar();
-		return FirstOf(
-				Sequence(QualifiedIdentifier(), children.get().add(new Import(match()))),
-				ImportSubDeclaration(parent, children));
-	}
-
-	public Rule ImportSubDeclaration(StringVar parent, Var<List<Import>> children) {
-		return Sequence(
-				this.symbolParser.OpenBracket(),
-				ImportMember(children),
-				ZeroOrMore(ImportSeparator(), ImportMember(children)),
-				Optional(this.whitespace.Spacing()),
-				this.symbolParser.CloseBracket());
-	}
-
-	public Rule ImportMember(Var<List<Import>> children) {
-		return Sequence(
-				QualifiedIdentifier(),
-				children.get().add(new Import(match())));
-	}
-
-	public Rule ImportSeparator() {
-		return this.symbolParser.Comma();
-	}
-
-	public Rule QualifiedIdentifier() {
-		return Sequence(
-				Optional(this.packageParser.Package(), this.packageParser.PackageSeparator()),
-				this.type.Type());
 	}
 
 }
