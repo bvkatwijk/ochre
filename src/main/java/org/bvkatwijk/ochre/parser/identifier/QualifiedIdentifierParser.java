f4d2222f83245e8e4eac624e3dca2fcb11a56cf4
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-20 16:52:07 +0200

    Add option for unqualified identifier

--- src/main/java/org/bvkatwijk/ochre/parser/identifier/QualifiedIdentifierParser.java
+++ src/main/java/org/bvkatwijk/ochre/parser/identifier/QualifiedIdentifierParser.java
@@ -1,5 +1,8 @@
 package org.bvkatwijk.ochre.parser.identifier;
 
+import java.util.ArrayList;
+
+import org.bvkatwijk.ochre.lang.pack.Package;
 import org.bvkatwijk.ochre.parser.pack.PackageParser;
 import org.bvkatwijk.ochre.parser.type.TypeReferenceParser;
 import org.parboiled.BaseParser;
@@ -9,19 +12,29 @@ import org.parboiled.Rule;
 public class QualifiedIdentifierParser extends BaseParser<QualifiedIdentifier> {
 
 	public final PackageParser packageParser = PackageParser.create();
-	public final TypeReferenceParser type = Parboiled.createParser(TypeReferenceParser.class);
+	public final TypeReferenceParser typeReferenceParser = TypeReferenceParser.create();
 
 	public Rule QualifiedIdentifier() {
-		return Sequence(
+		return FirstOf(
 				QualifiedIdentifierMatcher(),
-				push(new QualifiedIdentifier(match())));
+				Type());
 	}
 
+	// Are stack values actually shared between parsers?
 	public Rule QualifiedIdentifierMatcher() {
 		return Sequence(
-				Optional(this.packageParser.Package(),
-						this.packageParser.PackageSeparator()),
-				this.type.Type());
+				this.packageParser.Package(),
+				this.packageParser.PackageSeparator(),
+				this.typeReferenceParser.Type(),
+				push(new QualifiedIdentifier(this.packageParser.pop(2), this.typeReferenceParser.pop())));
+	}
+
+	public Rule Type() {
+		return Sequence(
+				this.typeReferenceParser.Type(),
+				push(new QualifiedIdentifier(
+						new Package(new ArrayList<>()),
+						this.typeReferenceParser.pop())));
 	}
 
 	public static QualifiedIdentifierParser create() {
