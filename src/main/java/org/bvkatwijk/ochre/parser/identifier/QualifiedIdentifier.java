e31f7ef0974605a838e6b16fbbd58e12f00466f4
Boris van Katwijk <bvkatwijk@gmail.com>
2018-07-20 16:11:54 +0200

    Let qualified identifier consist of package and type

--- src/main/java/org/bvkatwijk/ochre/parser/identifier/QualifiedIdentifier.java
+++ src/main/java/org/bvkatwijk/ochre/parser/identifier/QualifiedIdentifier.java
@@ -1,10 +1,28 @@
 package org.bvkatwijk.ochre.parser.identifier;
 
+import org.bvkatwijk.ochre.lang.pack.Package;
+import org.bvkatwijk.ochre.lang.type.Type;
+
 import lombok.Value;
 
 @Value
 public class QualifiedIdentifier {
 
-	String value;
+	Package pack;
+	Type type;
+
+	public QualifiedIdentifier prepend(Package parent) {
+		return new QualifiedIdentifier(this.pack.prepend(parent), this.type);
+	}
+
+	public static QualifiedIdentifier of(String typeName, String... packages) {
+		return new QualifiedIdentifier(
+				Package.of(packages),
+				new Type(typeName));
+	}
+
+	public String asString() {
+		return this.pack.getPrependValue() + this.type.getName();
+	}
 
 }
